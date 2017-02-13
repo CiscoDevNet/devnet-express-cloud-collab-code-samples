91401d3c893005d1ebcde99d42d3b27ed6694598
jocreed <jocreed@cisco.com>
2017-02-13 12:39:57 -0600

    corrected run_spark_bot.py, display name was not working correctly

--- itp/module07/07-lab04-run-spark-bot-locally/run_spark_bot.py
+++ itp/module07/07-lab04-run-spark-bot-locally/run_spark_bot.py
@@ -12,7 +12,7 @@ except ImportError as e:
     sys.exit()
 
 
-bearer = "ZThhNmZhMDktODEwZS00MzViLWE1M2QtNzc5NDAxOWNjOGNlODMwOGZlZjQtZjBh"  # BOT'S ACCESS TOKEN
+bearer = ""  # BOT'S ACCESS TOKEN
 headers = {
     "Accept": "application/json",
     "Content-Type": "application/json; charset=utf-8",
