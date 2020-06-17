41a349f57b9bf41317a9bf071d8fd5dd5dc4ad68
dstaudt <dstaudt@cisco.com>
2020-06-17 16:10:34 -0500

    collab-spark-mission-automate-sd - Updates to latest csv-parse and node-sparky - Replace ciscosparkpi with Webex Node SDK - Use launch.json for env secrets - Update sample bot names in data.csv - Replace 'spark' with 'webex' throughout - Use one package.json and npm install for the entire lab - Use path.join for paths - Use const instead of var

--- sd/collab-spark-mission-automate-sd/solutions/sparky/populate.js
+++ sd/collab-spark-mission-automate-sd/solutions/sparky/populate.js
@@ -1,46 +1,45 @@
-const assert = require('assert');
-assert(process.env.ACCESS_TOKEN, "Cannot populate without a Webex Teams API access token, aborting...");
+const assert = require( 'assert' );
+assert( process.env.WEBEX_ACCESS_TOKEN, 'Cannot populate without a Webex Teams API access token, aborting...' );
 
-var WebexTeamsClient = require("node-sparky");
-var sparky = new WebexTeamsClient({ token: process.env.ACCESS_TOKEN });
+const WebexTeamsClient = require( 'node-sparky' );
+const sparky = new WebexTeamsClient( { token: process.env.WEBEX_ACCESS_TOKEN } );
 
 // Read CSV sample: https://github.com/wdavidw/node-csv-parse/blob/master/samples/fs_read.js
-var fs = require('fs');
-var parse = require('csv-parse');
+const fs = require( 'fs' );
+const path = require( 'path' );
+const parse = require( 'csv-parse' );
 
-var parser = parse({ delimiter: ';', columns: true }, function (err, data) {
-  if (err) {
-    console.log("Sorry, coud not read CSV data, aborting...");
-    process.exit(1);
+const parser = parse( { delimiter: ';', columns: true }, function ( err, data ) {
+  if ( err ) {
+    console.log( 'Sorry, coud not read CSV data, aborting...' );
+    process.exit( 1 );
   }
 
   // Append participants to the room
   // [TODO] Create a space and place the identifier below
-  var roomToPopulate = "PASTE_YOUR_ROOMID";
-  data.forEach(function (elem, index) {
-    if (elem.email) {
+  const roomToPopulate = 'YOUR_ROOM_ID';
+  data.forEach( function ( elem, index ) {
+    if ( elem.email ) {
 
-      sparky.membershipAdd(roomToPopulate, elem.email, false)
-        .then(function(membership) {
-          console.log(`successfully added ${elem.email}`);
+      sparky.membershipAdd( roomToPopulate, elem.email, false )
+        .then( function( membership ) {
+          console.log( `successfully added ${ elem.email }` );
         })
-        .catch(function (error) {
-          console.log(`could not add member: ${elem.email}, err: ${error}`);
+        .catch( function ( error ) {
+          console.log( `could not add member: ${ elem.email }, err: ${ error }` );
         });
     }
   });
 });
 
-parser.on('error', function (err) {
-  console.log(err.message);
-});
-
-parser.on('finish', function () {
-  console.log("successfully parsed CSV file, continuing...");
-});
-
-
-// Launcher parsing
-fs.createReadStream(__dirname + '/../../data.csv').pipe(parser);
-
+parser.on( 'error', function ( err ) {
+    console.log( err.message );
+  });
+  
+  parser.on( 'finish', function () {
+    console.log( 'successfully parsed CSV file, continuing...' );
+  });
+  
+  // Launcher parsing
+  fs.createReadStream( path.join( __dirname, '..', '..', 'data.csv' ) ).pipe( parser );
 
